646f000e75bdbd9c89007e48c461c15479adaf0a
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-30 21:32:03 +0200

    fixed email send

--- www/register.php
+++ www/register.php
@@ -1,23 +1,28 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 $error = NULL;
 include('config.php');
 header('Content-Type: text/html;charset=UTF-8');
 
 
-if(!isset($_SESSION['register'])){
+if (!isset($_SESSION['register'])) {
   $_SESSION['register'] = 0;
 }
 
-if(isset($_SESSION['username']))
-{
+if (isset($_SESSION['username'])) {
   header("location: main-page.php");
-}
-else
-{
+} else {
   include('functions.php');
   //Clicking the register button
-  if(isset($_POST['register']))
-  {
+  if (isset($_POST['register'])) {
     $u = $_POST['username'];
     $e = $_POST['email'];
     $p = $_POST['password'];
@@ -27,11 +32,11 @@ else
     $ln = $_POST['ln'];
     $bday = $_POST['bday'];
     $gender = $_POST['gender'];
- 
-    //$u = mb_convert_encoding($u); 
+
+    //$u = mb_convert_encoding($u);
     //echo $u;
     //connecting to the database
-    
+
     $fn = $mysqli->real_escape_string($fn);
     $ln = $mysqli->real_escape_string($ln);
     $u = $mysqli->real_escape_string($u);
@@ -40,7 +45,7 @@ else
     $p2 = $mysqli->real_escape_string($p2);
     $bday = $mysqli->real_escape_string($bday);
     $gender = $mysqli->real_escape_string($gender);
-    //$u = mb_convert_encoding($u); 
+    //$u = mb_convert_encoding($u);
     //echo $u;
     $query = "SELECT * FROM users WHERE email = '$e'";
     //unique email testing
@@ -49,57 +54,66 @@ else
     $dateLimit = new DateTime('now');
     //errors
     //English starts
-    if(mysqli_num_rows($result) == 1)
-      $error .= "<p>".lang_check(array("This email is already taken!", "このメールはすでに届いています！!","Ez az email már foglalt!", "Ova email adresa već je zauzeta!"))."</p>";
-    
-    if(strlen($u) < 5)
-      $error .= "<p>".lang_check(array("Username must be at least 5 character!", "ユーザー名は5文字以上である必要があります！","A felhasználónév legalább 5 karakter kell, hogy legyen!", "Korisničko ime treba da sadrži najmanje 5 karaktera!"))."</p>";
-    
-    if(strlen($p) < 6)
-      $error .= "<p>".lang_check(array("Password must be at least 6 character!", "パスワードは6文字以上である必要があります！!","A jelszó legalább 6 karakter kell, hogy legyen!", "Lozinka treba da sadrži najmanje 6 karaktera!!"))."</p>";
+    if (mysqli_num_rows($result) == 1) {
+      $error .= "<p>" . lang_check(array("This email is already taken!", "このメールはすでに届いています！!", "Ez az email már foglalt!", "Ova email adresa već je zauzeta!")) . "</p>";
+    }
 
-    if (!preg_match("#^[a-zA-Z0-9]+$#", $p))
-      $error .= "<p>".lang_check(array("Please use only letters and numbers in your password!", "パスワードには文字と数字のみを使用してください","Kérlek csak számokat és betűket használj a jelszóban!", "Samo slova i brojeve može da sadrži lozinka!"))."</p>";
-    
-    if($p2 != $p)
-      $error .= "<p>".lang_check(array("Your passwords do not match!", "パスワードが一致しません。","Nem egyeznek a jelszavaid!", "Lozinke nisu isti!"))."</p>";
-    
-    else
-    {
-      //password hashing
-      $options = array("cost"=>4);
-      $hashPassword = password_hash($p,PASSWORD_BCRYPT,$options);
+    if (strlen($u) < 5) {
+      $error .= "<p>" . lang_check(array("Username must be at least 5 character!", "ユーザー名は5文字以上である必要があります！", "A felhasználónév legalább 5 karakter kell, hogy legyen!", "Korisničko ime treba da sadrži najmanje 5 karaktera!")) . "</p>";
+    }
 
-      //creating the verification
-      $vkey = PASSWORD_HASH(register . phptime() . $u,PASSWORD_DEFAULT);
-      /*if(function_exists('mysql_set_charset') ){
-        mysql_set_charset('utf8', $conn);
-      }else{
-        mysql_query("SET NAMES 'utf8'", $conn);
-      }*/
-      //inserting to database
-      $insert =$mysqli->query("INSERT INTO users(first_name,last_name,username,password,email,birthday,vkey,gender) VALUES('$fn','$ln','$u', '$hashPassword', '$e','$bday', '$vkey', '$gender')");
-      if($insert)
-      {
+    if (strlen($p) < 6) {
+      $error .= "<p>" . lang_check(array("Password must be at least 6 character!", "パスワードは6文字以上である必要があります！!", "A jelszó legalább 6 karakter kell, hogy legyen!", "Lozinka treba da sadrži najmanje 6 karaktera!!")) . "</p>";
+    }
 
+    if (!preg_match("#^[a-zA-Z0-9]+$#", $p)) {
+      $error .= "<p>" . lang_check(array("Please use only letters and numbers in your password!", "パスワードには文字と数字のみを使用してください", "Kérlek csak számokat és betűket használj a jelszóban!", "Samo slova i brojeve može da sadrži lozinka!")) . "</p>";
+    }
+
+    if ($p2 != $p) {
+      $error .= "<p>" . lang_check(array("Your passwords do not match!", "パスワードが一致しません。", "Nem egyeznek a jelszavaid!", "Lozinke nisu isti!")) . "</p>";
+    } else {
+      $options = array("cost" => 4);
+      $hashPassword = password_hash($p, PASSWORD_BCRYPT, $options);
+
+      $timestamp = time();
+      $vkey = password_hash('register' . $timestamp . $u, PASSWORD_DEFAULT);
+      $insert = $mysqli->query("INSERT INTO users(first_name,last_name,username,password,email,birthday,vkey,gender) VALUES('$fn','$ln','$u', '$hashPassword', '$e','$bday', '$vkey', '$gender')");
+      if ($insert) {
         $message_en = "You have to verify your email address, if you want to continue with your login.\nClick here to <a href='https://community.kreastol.club/verify.php?vkey=$vkey'>Verify Your Account</a>!";
         $message_jp = "メールアドレスを確認する必要があります。\nアカウントを<a href='https://community.kreastol.club/Community_Login/verify.php?vkey=$vkey'>確認する</a>には、ここをクリックしてください。";
         $message_hu = "Meg kell erősítened az email címed a folytatáshoz\nKattintson ide a <a href='https://community.kreastol.club/Community_Login/verify.php?vkey=$vkey'>Verifikáláshoz</a>!";
         $message_rs = "Trebate da verifikujete email adresu za naspasu.\nKliknite ovde za<a href='https://community.kreastol.club/Community_Login/verify.php?vkey=$vkey'>Verifikaciju</a>!";
 
-        $to = $e;
-        $subject = lang_check(array("Email verification for Kreastol Club","クレアストール・クラブのメールによる確認。","Email verifikáció a Kreastol Klub oldalra!","Email verifikacija za Kreastol Klub"));
-        $message = lang_check(array($message_en, $message_jp, $message_hu, $message_rs));
-        $headers = "From: noreply@mail.kreastol.club\r\n";
-        $headers .= "MIME-version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $mail = new PHPMailer(true);
 
-        mail($to,$subject,$message,$headers);
+        try {
+            // Take the password the sender end the host from the environment variables
+          $sender = getenv('SMTP_SENDER');
+          $mail->isSMTP();
+          $mail->Host = getenv('SMTP_HOST');
+          $mail->SMTPAuth = true;
+          $mail->Username = $sender;
+          $mail->Password = getenv('SMTP_PASS');
+          $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+          $mail->Port = 587;
+
+          $subject = lang_check(array("Email verification for Kreastol Club", "クレアストール・クラブのメールによる確認。", "Email verifikáció a Kreastol Klub oldalra!", "Email verifikacija za Kreastol Klub"));
+          $message = lang_check(array($message_en, $message_jp, $message_hu, $message_rs));
+
+          $mail->setFrom($sender, 'Kreastol Club');
+          $mail->addAddress($e);
+          $mail->Subject = $subject;
+          $mail->Body = $message;
+          $mail->AltBody = strip_tags($message);
+          $mail->send();
+
           $_SESSION['register'] = 1;
-      }
-      else
-      {
-            $error .= $mysqli->error;
+        } catch (Exception $e) {
+          $error .= $e->errorMessage();
+        }
+      } else {
+        $error .= $mysqli->error;
       }
     }
   }
@@ -108,17 +122,14 @@ else
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <?php
-  if($_SESSION['register'] == 0)
-  {
-    include('html/register.php');
-  }
-  else
-  {
-    include('html/thankyou.php');
-    $_SESSION['register'] = 0;
-  }
-  
-  ?>
+<?php
+if ($_SESSION['register'] == 0) {
+  include('html/register.php');
+} else {
+  include('html/thankyou.php');
+  $_SESSION['register'] = 0;
+}
+
+?>
 </body>
 </html>
--- www/verify.php
+++ www/verify.php
@@ -3,55 +3,49 @@ $error = NULL;
 session_start();
 include "functions.php";
 
-  
-if(isset($_GET['vkey']))
-{
-    $vkey = $_GET['vkey'];
-    $mysqli = NEW MySQLi('localhost','joshua','Kre@stolAda2003','Kreastol');
 
-    $resultSet = $mysqli->query("SELECT verified,vkey FROM users WHERE verified = 0 AND vkey = '$vkey' LIMIT 1");
-    //echo $resultSet;
+if (isset($_GET['vkey'])) {
+  $vkey = $_GET['vkey'];
+  // Get the variables from the environment variables
+  $db_user = getenv('DB_USER');
+  $db_pass = getenv('DB_PASS');
+  $db_host = getenv('DB_HOST');
+  $mysqli = new MySQLi($db_host, $db_user, $db_pass, 'Kreastol');
 
-    if($resultSet->num_rows == 1)
-    {
-        $update = $mysqli->query("UPDATE users SET verified = 1 WHERE vkey = '$vkey' LIMIT 1");
-        if($update)
-        {
+  $resultSet = $mysqli->query("SELECT verified,vkey FROM users WHERE verified = 0 AND vkey = '$vkey' LIMIT 1");
+  //echo $resultSet;
 
-            if($_SESSION['lang'] == "en")
-                $error ="<p>Your account has been verified you may now log in!</p>";
-            elseif($_SESSION['lang'] == "jp")
-                $error ="<p>アカウントが確認されました。これでログインできます。</p>";
-            elseif($_SESSION['lang'] == "hu")
-                $error ="<p>A fiókod meg lett erősitve, mostmár bejelentkezhetsz!</p>";
-            elseif($_SESSION['lang'] == "rs")
-            {
+  if ($resultSet->num_rows == 1) {
+    $update = $mysqli->query("UPDATE users SET verified = 1 WHERE vkey = '$vkey' LIMIT 1");
+    if ($update) {
 
-            }
-            $error .= "<embed id='mail' type='image/svg+xml' src='www/img/verify/verify.svg'/>";
-        }
-        else
-        {
-            $mysqli->error;
-        }
+      if ($_SESSION['lang'] == "en")
+        $error = "<p>Your account has been verified you may now log in!</p>";
+      elseif ($_SESSION['lang'] == "jp")
+        $error = "<p>アカウントが確認されました。これでログインできます。</p>";
+      elseif ($_SESSION['lang'] == "hu")
+        $error = "<p>A fiókod meg lett erősitve, mostmár bejelentkezhetsz!</p>";
+      elseif ($_SESSION['lang'] == "rs") {
+
+      }
+      $error .= "<embed id='mail' type='image/svg+xml' src='img/verify/verify.svg'/>";
+    } else {
+      $mysqli->error;
     }
-    else{
-        if($_SESSION['lang'] == "en")
-            $error .= "<p>This account is invalid or already verified</p>";
-        elseif($_SESSION['lang'] == "jp")
-            $error .= "<p>このアカウントは無効であるか、すでに確認済みです。</p>";
-        elseif($_SESSION['lang'] == "hu")
-            $error .= "<p>Ez a fiók érvénytelen, vagy már meg van erősítve!</p>";
-        elseif($_SESSION['lang'] == "rs")
-        {
+  } else {
+    if ($_SESSION['lang'] == "en")
+      $error .= "<p>This account is invalid or already verified</p>";
+    elseif ($_SESSION['lang'] == "jp")
+      $error .= "<p>このアカウントは無効であるか、すでに確認済みです。</p>";
+    elseif ($_SESSION['lang'] == "hu")
+      $error .= "<p>Ez a fiók érvénytelen, vagy már meg van erősítve!</p>";
+    elseif ($_SESSION['lang'] == "rs") {
 
-        }
-        $error .= "<embed id='mail' type='image/svg+xml' src='www/img/verify/error.svg'/>";
     }
-}
-else
-{
-    die("Something went wrong");
+    $error .= "<embed id='mail' type='image/svg+xml' src='img/verify/error.svg'/>";
+  }
+} else {
+  die("Something went wrong");
 }
 ?>
 <!DOCTYPE html>
@@ -60,38 +54,32 @@ else
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/thankyou.css">
-    <meta property = "og: image" content = "https://kreastol.club/Kreastol/img/Page-Logo.png"/>
-    <link rel="icon" href="https://kreastol.club/Kreastol/img/Title-Logo.png"> 
+    <link rel="stylesheet" href="css/colors-schemes.css" type=text/css>
+    <meta property="og: image" content="img/Page-Logo.png"/>
+    <link rel="icon" href="img/Title-Logo.png">
     <title>Verification</title>
 </head>
 <body>
-    <div class="container">
-    <?php
-        if($_SESSION['lang'] == "en")
-        {
-            echo "<h3>Verifying!</h3>
+<div class="container">
+  <?php
+  if ($_SESSION['lang'] == "en") {
+    echo "<h3>Verifying!</h3>
             $error
-            <br><button id=\"login\" onclick=\"window.location.href='https://community.kreastol.club/login.php';\">Log In</button>";
-        }
-        elseif($_SESSION['lang'] == "jp")
-        {
-            echo "<h3>確認中！</h3>
+            <br><button id=\"login\" onclick=\"window.location.href='/login.php';\">Log In</button>";
+  } elseif ($_SESSION['lang'] == "jp") {
+    echo "<h3>確認中！</h3>
             $error
-            <br><button id=\"login\" onclick=\"window.location.href='https://community.kreastol.club/login.php';\">ログインする</button>";
-        }
-        elseif($_SESSION['lang'] == "hu")
-        {
-            echo ">Bejelentkezés</button>";
-        }
-        elseif($_SESSION['lang'] == "rs")
-        {
-            echo "<h3></h3>
+            <br><button id=\"login\" onclick=\"window.location.href='/login.php';\">ログインする</button>";
+  } elseif ($_SESSION['lang'] == "hu") {
+    echo ">Bejelentkezés</button>";
+  } elseif ($_SESSION['lang'] == "rs") {
+    echo "<h3></h3>
             $error
-            <br><button id=\"login\" onclick=\"window.location.href='https://community.kreastol.club/login.php';\"></button>";
-        }
+            <br><button id=\"login\" onclick=\"window.location.href='/login.php';\"></button>";
+  }
 
-    ?>
-    
-    </div>
+  ?>
+
+</div>
 </body>
 </html>
